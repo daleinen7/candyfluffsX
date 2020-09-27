db5f5000d2f8194bc427ab6696768773245e6c19
BrittGall <crlnfllr@gmail.com>
2020-09-27 01:53:16 -0500

    added the routes for about & conventions,

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'), 
+    path('about/', views.about, name='about'), 
+    path('conventions/', views.conventions, name='conventions'), 
     path('products', views.ProductsList.as_view(), name='products'),
     path('products/<int:pk>/', views.ProductDetail.as_view(), name='product_detail'),
     path('accounts/signup/', views.signup, name='signup'),
