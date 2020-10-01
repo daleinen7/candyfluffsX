a6952b0fd055d5505546cfa510761912099ae05f
BrittGall <crlnfllr@gmail.com>
2020-10-01 00:56:43 -0500

    added models for featured images and live events

--- main_app/models.py
+++ main_app/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-#### ADD FANDOMS HERE * KEYS MUST BE UNIQUE *
+# ADD FANDOMS HERE * KEYS MUST BE UNIQUE *
 FANDOMS = (
     ('H', 'My Hero Academia'),
     ('M', 'Miraculous Lady'),
@@ -15,13 +15,15 @@ FANDOMS = (
     ('C', 'Castlevania')
 )
 
-#### ADD PRODUCT TYPES HERE * KEYS MUST BE UNIQUE *
+# ADD PRODUCT TYPES HERE * KEYS MUST BE UNIQUE *
 PRODUCT_TYPES = (
     ('M', 'Misc'),
     ('B', 'Buttons')
 )
 
 # Create your models here.
+
+
 class Product(models.Model):
     title = models.CharField(max_length=270)
     price = models.IntegerField()
@@ -32,8 +34,26 @@ class Product(models.Model):
     variation = models.CharField(max_length=200)
     stock = models.IntegerField()
     sku = models.IntegerField()
-    weight = models.IntegerField() # is integer right here? yes, the weight will be in ounces as whole numbers!
+    # is integer right here? yes, the weight will be in ounces as whole numbers!
+    weight = models.IntegerField()
     size = models.CharField(max_length=200)
     created_date = models.DateTimeField('Created on')
 
+# featured images for homepage
 
+
+class Banner(models.Model):
+    title = models.CharField(max_length=270)
+    featured = models.ImageField()
+    created_date = models.DateTimeField('Created on')
+
+# Convention and live events
+
+
+class AnimeCon(models.Model):
+    eventName = models.CharField(max_length=270)
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    eventTicets = models.URLField()
+    description = models.TextField()
+    created_date = models.DateTimeField('Created on')
